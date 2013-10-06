fb9063d54036a3b9426e80a5de332c2120d8ebaa
Mikkel Skare <mikkel.skare@gmail.com>
2013-10-06 14:48:23 +1000

    2.8 (Mikkel)

--- KMKY/src/com/kmky/data/Calculate.java
+++ KMKY/src/com/kmky/data/Calculate.java
@@ -26,36 +26,48 @@ public class Calculate {
 
     public Drawable my_calls(int outgoing, int incoming, Context context)
     {
+
         Drawable drawable = null;
 
         int outgoing_treshold = (outgoing/(outgoing + incoming)) * 100;
 
+//        if ((outgoing_treshold < 1)){
+//            drawable = context.getResources().getDrawable(R.drawable.blank);
+//        }
+
         if ((outgoing_treshold > 1) && (outgoing_treshold <= 20))
         {
             drawable = context.getResources().getDrawable(R.drawable.insideheart30);
+            return  drawable;
         }
 
         else if ((outgoing_treshold > 20) && (outgoing_treshold <= 40))
         {
             drawable = context.getResources().getDrawable(R.drawable.insideheart35);
+            return  drawable;
         }
 
         else if ((outgoing_treshold > 40) && (outgoing_treshold <= 60))
         {
             drawable = context.getResources().getDrawable(R.drawable.insideheart40);
+            return  drawable;
         }
 
         else if ((outgoing_treshold > 60) && (outgoing_treshold <= 80))
         {
             drawable = context.getResources().getDrawable(R.drawable.insideheart45);
+            return  drawable;
         }
 
-        else
+        else if ((outgoing_treshold > 80) && (outgoing_treshold <= 100))
         {
             drawable = context.getResources().getDrawable(R.drawable.insideheart50);
+            return  drawable;
         }
 
-        return  drawable;
+        else { drawable = context.getResources().getDrawable(R.drawable.blank);
+            return drawable;}
+
     }
 
     public Drawable my_sms(int outgoing, int incoming, Context context)
@@ -68,31 +80,41 @@ public class Calculate {
         if ((outgoing_treshold > 1) && (outgoing_treshold <= 20))
         {
             drawable = context.getResources().getDrawable(R.drawable.outsideheart60);
+            return  drawable;
         }
 
         else if ((outgoing_treshold > 20) && (outgoing_treshold <= 40))
         {
             drawable = context.getResources().getDrawable(R.drawable.outsideheart70);
+            return  drawable;
         }
 
         else if ((outgoing_treshold > 40) && (outgoing_treshold <= 60))
         {
             drawable = context.getResources().getDrawable(R.drawable.outsideheart80);
+            return  drawable;
         }
 
         else if ((outgoing_treshold > 60) && (outgoing_treshold <= 80))
         {
             drawable = context.getResources().getDrawable(R.drawable.outsideheart90);
+            return  drawable;
         }
 
-        else
+        else if ((outgoing_treshold > 80) && (outgoing_treshold <= 100))
         {
             drawable = context.getResources().getDrawable(R.drawable.outsideheart100);
+            return  drawable;
         }
 
-        return  drawable;
-
+        else {
+            drawable = context.getResources().getDrawable(R.drawable.blank);
+            return drawable;
+        }
     }
+
+
+
     //
     public Drawable your_calls(int incoming, int outgoing, Context context)
     {
@@ -103,29 +125,38 @@ public class Calculate {
         if ((outgoing_treshold > 1) && (outgoing_treshold <= 20))
         {
             drawable = context.getResources().getDrawable(R.drawable.insideheart30);
+            return  drawable;
         }
 
         else if ((outgoing_treshold > 20) && (outgoing_treshold <= 40))
         {
             drawable = context.getResources().getDrawable(R.drawable.insideheart35);
+            return  drawable;
         }
 
         else if ((outgoing_treshold > 40) && (outgoing_treshold <= 60))
         {
             drawable = context.getResources().getDrawable(R.drawable.insideheart40);
+            return  drawable;
         }
 
         else if ((outgoing_treshold > 60) && (outgoing_treshold <= 80))
         {
             drawable = context.getResources().getDrawable(R.drawable.insideheart45);
+            return  drawable;
         }
 
-        else
+        else if ((outgoing_treshold > 80) && (outgoing_treshold <= 100))
         {
             drawable = context.getResources().getDrawable(R.drawable.insideheart50);
+            return  drawable;
         }
 
-        return  drawable;
+        else {
+            drawable = context.getResources().getDrawable(R.drawable.blank);
+            return drawable;
+        }
+
     }
 
     public Drawable your_sms(int incoming, int outgoing, Context context)
@@ -137,29 +168,38 @@ public class Calculate {
         if ((outgoing_treshold > 1) && (outgoing_treshold <= 20))
         {
             drawable = context.getResources().getDrawable(R.drawable.outsideheart60);
+            return drawable;
         }
 
         else if ((outgoing_treshold > 20) && (outgoing_treshold <= 40))
         {
             drawable = context.getResources().getDrawable(R.drawable.outsideheart70);
+            return drawable;
         }
 
         else if ((outgoing_treshold > 40) && (outgoing_treshold <= 60))
         {
             drawable = context.getResources().getDrawable(R.drawable.outsideheart80);
+            return drawable;
         }
 
         else if ((outgoing_treshold > 60) && (outgoing_treshold <= 80))
         {
             drawable = context.getResources().getDrawable(R.drawable.outsideheart90);
+            return drawable;
         }
 
-        else
+        else if ((outgoing_treshold > 80) && (outgoing_treshold <= 100))
         {
-            drawable = context.getResources().getDrawable(R.drawable.outsideheart100);
+            drawable = context.getResources().getDrawable(R.drawable.outsideheart90);
+            return drawable;
         }
 
-        return  drawable;
+        else {
+            drawable = context.getResources().getDrawable(R.drawable.blank);
+            return drawable;
+        }
+
     }
 
 }
